0d611aaa3b08c4a0021756928cdbc84153fe8e01
Casey Boettcher <casey@learningequality.org>
2021-06-04 02:55:02 -0400

    Disabling Meltano native AuthN

--- entrypoint.py
+++ entrypoint.py
@@ -71,8 +71,8 @@ if __name__ == "__main__":
         print(os.environ)
     print("Running meltano install...")
     subprocess.run(['meltano', 'install'])
-    print("Adding admin user...")
-    subprocess.run(['meltano', 'user', 'add', '--role', 'admin', '--overwrite',
-                    os.getenv('MELTANO_ADMIN_USER'), os.getenv('MELTANO_ADMIN_PASSWORD')])
+    #print("Adding admin user...")
+    #subprocess.run(['meltano', 'user', 'add', '--role', 'admin', '--overwrite',
+    #                os.getenv('MELTANO_ADMIN_USER'), os.getenv('MELTANO_ADMIN_PASSWORD')])
     print("Running meltano ui...")
     subprocess.run(['meltano', '-v', '--log-level', logging, 'ui'])
